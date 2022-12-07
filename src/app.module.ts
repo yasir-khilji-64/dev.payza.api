66d7f84124c9e5c3f47cf1c24b74f1805f34c67c
Yasir Hassan <yasir.hassan1993@hotmail.com>
2022-12-07 17:00:26 +0500

    [WIP] Added port configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${process.env.NODE_ENV}`],
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    await app.listen(port, () => {
+      console.log(`Server listening on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
 bootstrap();
